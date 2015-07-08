78eb1fb8b2f6e869564513cd9808e1c434506996
Bilal ATOUI <batoui@octo.com>
2015-07-08 14:42:34 +0000

    Duplication des contacts

--- Owncloud iOs Client/Tabs/SettingTab/ImportContactsDstViewController/ImportContactsDstViewController.m
+++ Owncloud iOs Client/Tabs/SettingTab/ImportContactsDstViewController/ImportContactsDstViewController.m
@@ -172,9 +172,164 @@
 
 -(void) beginContactsCopy {
     
-    NSLog(@"choosedSources:%@", choosedSources);
-    NSLog(@"filteredDestinations:%@", filteredDestinations);
+    NSArray * theSelectedSources = [self getContactsImportSources];
+    NSDictionary * theSelectedDestination = [self getContactsImportDestination];
     
+    NSLog(@"theSelectedSources:%@", theSelectedSources);
+    NSLog(@"theSelectedDestination:%@", theSelectedDestination);
+    
+    [self importContactTo:theSelectedDestination from:theSelectedSources];
+    [self.navigationController popToRootViewControllerAnimated:YES];
+    
+}
+
+- (void)importContactTo:(NSDictionary *)theSelectedDestination from:(NSArray * )theSelectedSources{
+    
+    // Get the addressBook
+    CFErrorRef error = NULL;
+    ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, &error);
+    
+    if (error != NULL) {
+        NSLog(@"iPhoneAddressBook not created");
+        return;
+    }
+    
+    //Get the DestinationSourceRef
+    CFArrayRef AllSourcesRef = ABAddressBookCopyArrayOfAllSources(addressBook);
+    ABRecordRef destinationSourceRef = [self getDestinationSourceFromSources:AllSourcesRef identifiedWith:theSelectedDestination];
+    
+    if(destinationSourceRef == NULL) {
+        
+        NSLog(@"destinationSourceRef not found");
+        CFRelease(AllSourcesRef);
+        CFRelease(addressBook);
+        return;
+    }
+    
+    //Get The SourcesSourceRef
+    for(int i = 0; i < [theSelectedSources count]; i++) {
+        
+        NSDictionary *aSelectedSource = [theSelectedSources objectAtIndex:i];
+        
+        CFIndex countSourcesRef = CFArrayGetCount(AllSourcesRef);
+        for(CFIndex idx=0; idx < countSourcesRef; ++idx) {
+            
+            ABRecordRef aSourceRef = CFArrayGetValueAtIndex(AllSourcesRef, idx);
+            NSNumber *type = (NSNumber *)CFBridgingRelease(ABRecordCopyValue(aSourceRef, kABSourceTypeProperty));
+            NSString *name = (NSString *)CFBridgingRelease(ABRecordCopyValue(aSourceRef, kABSourceNameProperty));
+            NSNumber *selectedType = [aSelectedSource objectForKey:@"type"];
+            NSString *selectedName = [aSelectedSource objectForKey:@"name"];
+            
+            NSLog(@"Is selectedType:%@ selectedName:%@",selectedType, selectedName);
+            NSLog(@"A match for type:%@ name:%@",type, name);
+            
+            if( ([type intValue] == [selectedType intValue]) && ([name isEqualToString:selectedName]) ){
+                
+                [self copyAddressBookContacts:addressBook
+                                   fromSource:aSourceRef toSource:destinationSourceRef];
+            }
+            
+            if( ([type intValue] == [selectedType intValue]) && ([type intValue] == 0) ){
+                
+                [self copyAddressBookContacts:addressBook
+                                   fromSource:aSourceRef toSource:destinationSourceRef];
+            }
+        }
+    }
+    
+    CFRelease(AllSourcesRef);
+    CFRelease(addressBook);
+}
+
+-(ABRecordRef)getDestinationSourceFromSources:(CFArrayRef)AllSourcesRef identifiedWith:(NSDictionary *)theSelectedDestination {
+    
+    ABRecordRef destinationSourceRef = NULL;
+    
+    CFIndex countSourcesRef = CFArrayGetCount(AllSourcesRef);
+    
+    for(CFIndex idx=0; idx < countSourcesRef; ++idx) {
+        
+        ABRecordRef aSourceRef = CFArrayGetValueAtIndex(AllSourcesRef, idx);
+        NSNumber *type = (NSNumber *)CFBridgingRelease(ABRecordCopyValue(aSourceRef, kABSourceTypeProperty));
+        NSString *name = (NSString *)CFBridgingRelease(ABRecordCopyValue(aSourceRef, kABSourceNameProperty));
+        NSNumber *selectedType = [theSelectedDestination objectForKey:@"type"];
+        NSString *selectedName = [theSelectedDestination objectForKey:@"name"];
+        
+        if( ([type intValue] == [selectedType intValue]) && ([name isEqualToString:selectedName]) ){
+            
+            NSLog(@"destinationSourceRef found");
+            destinationSourceRef = aSourceRef;
+            break;
+        }
+    }
+    
+    return destinationSourceRef;
+}
+
+-(void) copyAddressBookContacts:(ABAddressBookRef)addressBook
+                     fromSource:(ABRecordRef)fromSourceRef toSource:(ABRecordRef)toSourceRef {
+    
+    NSString *fromSourceRefName = (NSString *)CFBridgingRelease(ABRecordCopyValue(fromSourceRef, kABSourceNameProperty));
+    NSString *toSourceRefName = (NSString *)CFBridgingRelease(ABRecordCopyValue(toSourceRef, kABSourceNameProperty));
+    
+    NSLog(@"Importing contacts from %@ to %@",fromSourceRefName, toSourceRefName);
+    
+    CFArrayRef personsToImport = ABAddressBookCopyArrayOfAllPeopleInSource (addressBook, fromSourceRef);
+    CFDataRef vCardDataRef = ABPersonCreateVCardRepresentationWithPeople(personsToImport);
+    CFArrayRef createdPersons = ABPersonCreatePeopleInSourceWithVCardRepresentation (toSourceRef, vCardDataRef);
+    
+    
+    CFIndex createdPersonsCount = CFArrayGetCount(createdPersons);
+    for(CFIndex idx=0; idx < createdPersonsCount; ++idx) {
+        
+        ABRecordRef newPerson = CFArrayGetValueAtIndex(createdPersons, idx);
+        ABAddressBookAddRecord(addressBook, newPerson, nil);
+        ABAddressBookSave(addressBook, nil);
+    }
+}
+
+-(NSDictionary *) getContactsImportDestination {
+    
+    NSMutableDictionary *theSelectedDestination = [NSMutableDictionary dictionaryWithCapacity:10];
+    
+    for(int i = 0; i < [self.filteredDestinations count]; i++) {
+        
+        NSMutableArray *aDst = [self.filteredDestinations objectAtIndex:i];
+        NSNumber *isSelected = [aDst objectAtIndex:2];
+        if([isSelected intValue] == 1) {
+            
+            NSNumber *type = [aDst objectAtIndex:0];
+            NSString *name = [aDst objectAtIndex:1];
+            [theSelectedDestination setObject:type forKey:@"type"];
+            [theSelectedDestination setObject:name forKey:@"name"];
+            
+            break;
+        }
+    }
+    
+    return theSelectedDestination;
+}
+
+-(NSArray *) getContactsImportSources {
+    
+    NSMutableArray *theSelectedSources = [NSMutableArray arrayWithCapacity:10];
+    
+    for(int i = 0; i < [self.choosedSources count]; i++) {
+        
+        NSArray *aSource = [self.choosedSources objectAtIndex:i];
+        NSNumber *isSelected = [aSource objectAtIndex:2];
+        if([isSelected intValue] == 1) {
+            
+            NSNumber *type = [aSource objectAtIndex:0];
+            NSString *name = [aSource objectAtIndex:1];
+            NSMutableDictionary * aSourceDico = [NSMutableDictionary dictionaryWithCapacity:10];
+            [aSourceDico setObject:type forKey:@"type"];
+            [aSourceDico setObject:name forKey:@"name"];
+            [theSelectedSources addObject:aSourceDico];
+        }
+    }
+    
+    return theSelectedSources;
 }
 
 
